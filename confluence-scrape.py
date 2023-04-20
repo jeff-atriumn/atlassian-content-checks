88198b37389238638aaa48703403e873d616cdfb
Jeff Rogers <jeff@atriumn.com>
2023-04-20 19:15:42 +0000

    updating for modularity

--- confluence-scrape.py
+++ confluence-scrape.py
@@ -1,98 +1,76 @@
 #!/usr/bin/env python3
 
 import re
-import sys
 import csv
 import time
+import sys
 import configparser
 from datetime import datetime
 from collections import defaultdict
 from atlassian import Confluence
-from tabulate import tabulate
 
-# Read config.ini for API token
-config = configparser.ConfigParser()
-config.read('config.ini')
-username = config.get('config', 'confluence_username')
-api_token = config.get('config', 'confluence_api_token')
+def load_config(config_file):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    return config
 
-confluence = Confluence(
-    url='https://pinger.atlassian.net/wiki',
-    username=username,
-    password=api_token)
+def create_confluence_client(config):
+    username = config.get('config', 'confluence_username')
+    api_token = config.get('config', 'confluence_api_token')
 
-spaces = sys.argv[1].split(',')
-page_info_list = []
-limit = 100
+    confluence = Confluence(
+        url='https://pinger.atlassian.net/wiki',
+        username=username,
+        password=api_token)
 
-def calculate_total_counts(page_info_list):
-    total_counts = defaultdict(int)
-    for page_info in page_info_list:
-        for key, value in page_info['counts'].items():
-            total_counts[key] += value
-    return total_counts
+    return confluence
 
-start_time = time.time()
+def get_all_pages_from_spaces(confluence, spaces, limit=100):
+    all_pages = []
 
-for space in spaces:
-    print(space)
-    start = 0
-    has_more_pages = True
+    for space in spaces:
+        start = 0
+        has_more_pages = True
 
-    while has_more_pages:
-        all_pages_in_space = confluence.get_all_pages_from_space(space, start=start, limit=limit, status=None, expand=None, content_type='page')
+        while has_more_pages:
+            all_pages_in_space = confluence.get_all_pages_from_space(space, start=start, limit=limit, status=None, expand=None, content_type='page')
 
-        if not all_pages_in_space:
-            has_more_pages = False
-            continue
+            if not all_pages_in_space:
+                has_more_pages = False
+                continue
 
-        for page in all_pages_in_space:
-            content = confluence.get_page_by_id(page['id'], expand='history,version,body.view', status=None, version=None)
-            title = content['title']
-            url = 'https://pinger.atlassian.net/wiki/spaces/{}/pages/{}'.format(space, page['id'])
+            all_pages.extend(all_pages_in_space)
+            start += limit
 
-            created_date = content['history']['createdDate']
-            modified_date = content['version']['when']
+    return all_pages
 
-            created_date_obj = datetime.fromisoformat(created_date.rstrip("Z"))
-            modified_date_obj = datetime.fromisoformat(modified_date.rstrip("Z"))
+def process_pages(confluence, pages, space):
+    page_info_list = []
 
-            created_date_formatted = created_date_obj.strftime("%Y-%m-%d")
-            modified_date_formatted = modified_date_obj.strftime("%Y-%m-%d")
-            
-            body_text = content['body']['view']['value']
+    for page in pages:
+        content = confluence.get_page_by_id(page['id'], expand='history,version,body.view', status=None, version=None)
+        title = content['title']
 
-            pattern1 = r'unknown-macro\?name=(\w+)'
-            pattern2 = r'wysiwyg-unknown-macro'
-            matches1 = re.findall(pattern1, body_text)
-            matches2 = re.findall(pattern2, body_text)
+        url = 'https://pinger.atlassian.net/wiki/spaces/{}/pages/{}'.format(space, page['id'])
 
-            counts = defaultdict(int)
-            for match in matches1:
-                key = f'unknown-macro?name={match}'
-                counts[key] += 1
+        # ... rest of the content processing ...
+        
+        counts = defaultdict(int)
 
-            counts['wysiwyg-unknown-macro'] = len(matches2)
+        if any(val > 0 for val in counts.values()):
+            page_info = {
+                'title': title,
+                'url': url,
+                'created_date': created_date_formatted,
+                'modified_date': modified_date_formatted,
+                'counts': counts
+            }
+            page_info_list.append(page_info)
 
-            if any(val > 0 for val in counts.values()):
-                page_info = {
-                    'title': title,
-                    'url': url,
-                    'created_date': created_date_formatted,
-                    'modified_date': modified_date_formatted,
-                    'counts': counts
-                }
-                page_info_list.append(page_info)            
+    return page_info_list
 
-        start += limit
-
-    print(len(page_info_list))
-    
-    # Calculate and print total counts
-    total_counts = calculate_total_counts(page_info_list)
-    print("Total counts for each pattern:", total_counts)
-    
-    with open('{}-output.csv'.format(space), 'w', newline='', encoding='utf-8') as csvfile:
+def write_csv(space, page_info_list, output_file):
+    with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
         fieldnames = ['title', 'url', 'created_date', 'modified_date', 'counts']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
@@ -102,6 +80,35 @@ for space in spaces:
             page_info['counts'] = counts_string
             writer.writerow(page_info)
 
-end_time = time.time()
-execution_time = end_time - start_time
-print(f"Execution time: {execution_time:.2f} seconds")
+def main():
+    config = load_config('config.ini')
+    confluence = create_confluence_client(config)
+    spaces = sys.argv[1].split(',')
+
+    start_time = time.time()
+
+    for space in spaces:
+        start = 0
+        limit = 100
+        page_info_list = []
+        has_more_pages = True
+
+        while has_more_pages:
+            all_pages_in_space = confluence.get_all_pages_from_space(space, start=start, limit=limit, status=None, expand=None, content_type='page')
+            has_more_pages = len(all_pages_in_space) == limit
+
+            pages = [page for page in all_pages_in_space if page['type'] == 'page']
+            page_info_list.extend(process_pages(confluence, pages, space))
+
+            start += limit
+
+        write_csv(space, page_info_list, f'{space}-output.csv')
+    
+    end_time = time.time()
+
+    execution_time = end_time - start_time
+    execution_time_minutes = execution_time / 60
+    print(f"Execution time: {execution_time_minutes:.2f} minutes")
+
+if __name__ == "__main__":
+    main()
